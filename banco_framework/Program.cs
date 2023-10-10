7a41959f64ee93ee9021d3257c812913586a1c7d
Italo <italocovas@gmail.com>
2023-10-10 10:50:41 -0300

    Finalizando atividade prática System.IO ...

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Model;
+using Application;
 
 internal class Program
 {
@@ -8,26 +9,62 @@ internal class Program
         Console.WriteLine("Seja bem vindo ao banco Framework");
         Console.WriteLine("Por favor, identifique-se");
         Console.WriteLine("");
-        var pessoa = Identificacao();
+        var cliente = Identificacao();
     }
 
-    static Pessoa Identificacao()
+    static void Menu(Cliente cliente) {
+        Console.WriteLine("");
+        Console.WriteLine("--------------");
+        Console.WriteLine("Menu");
+        Console.WriteLine("--------------");
+        Console.WriteLine("1 - Depósito");
+        Console.WriteLine("2 - Saque");
+        Console.WriteLine("Digite sua opção:");
+        Console.WriteLine("");
+        int opcao = int.Parse(Console.ReadLine());
+        var calculo = new Calculo();
+
+        float valor = 0;
+
+        switch (opcao)
+        {
+            case 1:
+             Console.Clear();
+             Console.WriteLine("Digite o valor:");
+             valor = float.Parse(Console.ReadLine());
+             cliente.Saldo = calculo.Soma(cliente.Saldo, valor);
+             Console.WriteLine("");
+             Console.WriteLine($"==> Saldo atual é R${cliente.Saldo}");
+             Menu(cliente);
+             break;
+
+             case 2:
+             Console.Clear();
+             Console.WriteLine("Digite o valor:");
+             valor = float.Parse(Console.ReadLine());
+             cliente.Saldo = calculo.Subtracao(cliente.Saldo, valor);
+             Console.WriteLine("");
+             Console.WriteLine($"==> Saldo atual é R${cliente.Saldo}");
+             Menu(cliente);
+             break;
+            
+            default:
+            Console.Clear();
+            Console.WriteLine("Digite uma opção válida");
+            break;
+        }
+
+    }
+
+    static Cliente Identificacao()
     {
-        var pessoa = new Pessoa();
+        var cliente = new Cliente();
 
-        Console.WriteLine("Seu número de identificação:");
-        pessoa.Id = int.Parse(Console.ReadLine());
+        Console.WriteLine("Seu saldo:");
+        cliente.Saldo = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Seu nome:");
-        pessoa.Nome = Console.ReadLine();
-
-        Console.WriteLine("Seu CPF:");
-        pessoa.Cpf = Console.ReadLine();
-        Console.Clear();
-
-        Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        Console.ReadKey();
+        Menu(cliente);
         
-        return pessoa;
+        return cliente;
     }
 }
